841ea44f721786db124438b1b950a98860511b66
AnliClark <leclark9999@gmail.com>
2024-06-20 18:11:28 +0800

    目前想要解决粘包问题

--- register_center.py
+++ register_center.py
@@ -6,6 +6,7 @@ import pickle
 # todo password
 """
 说明：本文件为注册中心的文件
+规定发送的消息都具有2 bytes，大端存储的头部，用于存储消息长度
 规定server发送给注册中心的消息的格式为：
 request_data{
     'type': String  # 'register' or 'unregister'      
@@ -35,7 +36,10 @@ service_server_dict = {}
 
 def register_service(server_name, service_name, service_addr):
     server_addr_dict[server_name] = service_addr
-    service_server_dict[service_name] = server_name  # todo
+    if service_name in service_server_dict:
+        service_server_dict[service_name].append(server_name)
+    else:
+        service_server_dict[service_name] = [server_name]
     return True
 
 
@@ -43,14 +47,15 @@ def find_service(service_name):
     if service_name not in service_server_dict:
         service_addr = None
     else:
-        server_name = service_server_dict[service_name]  # todo
-        service_addr = server_addr_dict[server_name]
+        server_names = service_server_dict[service_name]
+        service_addr = server_addr_dict[server_names[0]]   # todo 负载均衡
     return service_addr
 
 
 def handle_request(acceptSocket):
     try:
         request_data = acceptSocket.recv(1024)
+        requ_len = int.from_bytes(request_data[:2], 'big', signed=False)
         request_data = pickle.loads(request_data)
         if request_data['type'] == 'register':
             server_name = request_data['server_name']
@@ -62,6 +67,8 @@ def handle_request(acceptSocket):
             service_addr = find_service(service_name)
             response_data = {'service_addr': service_addr}
         response_data = pickle.dumps(response_data)
+        resp_len = len(response_data)
+        response_data = resp_len.to_bytes(2, 'big', signed=False) + response_data
         acceptSocket.sendall(response_data)
     except Exception as e:
         print(e)
